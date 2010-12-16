76788f1a8089c76f6a5decff4a42b2b84584517e
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2010-12-16 00:57:45 -0200

    Schema para Data<->Comment.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ lib/OpenData/BR/Schema/Result/DataComment.pm
@@ -0,0 +1,54 @@
+package OpenData::BR::Schema::Result::DataComment;
+
+use strict;
+use warnings;
+
+use base 'DBIx::Class::Core';
+
+__PACKAGE__->table("data_comment");
+__PACKAGE__->load_components(qw/Core InflateColumn::DateTime ForceUTF8/);
+
+__PACKAGE__->add_columns(
+    "id",
+    {
+        data_type => "INT",
+        size => 11,
+        is_nullable => 0,
+        is_auto_increment => 1,
+    },
+    "user_id",
+    {
+        data_type => "INT",
+        default_value => undef
+    },
+    "data_id",
+    {
+        data_type => "INT",
+        default_value => undef
+    },
+    "description",
+    {
+        data_type => "longtext",
+    },
+    "create_time",
+    {
+        data_type => "timestamp",
+        size => 4,
+        default => \'NOW()',
+    },
+);
+
+__PACKAGE__->set_primary_key("id");
+
+__PACKAGE__->belongs_to(
+    user => 'OpenData::BR::Schema::Result::User' => 
+        { 'foreign.id' => 'self.user_id' }
+);
+
+__PACKAGE__->belongs_to(
+    data => 'OpenData::BR::Schema::Result::Data' => 
+        { 'foreign.id' => 'self.data_id' }
+);
+
+1;
+
